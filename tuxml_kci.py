c33067dc8d46f35bdeef4dc700d21d4d67dfdf0e
Nicolas Jay <jaynicolas@orange.fr>
2021-04-07 17:57:20 +0200

    add test command missing some argument

--- tuxml_kci.py
+++ tuxml_kci.py
@@ -652,11 +652,9 @@ if __name__ == "__main__":
 
     shutil.rmtree(extraction_path)
 
-    #print_flush("Build of {b_env}_{arch} complete.".format(b_env=b_env,arch=arch))
+    print_flush("Build of {b_env}_{arch} complete.".format(b_env=b_env,arch=arch))
 
-    os.chdir("/kernelci_core/")
-    print("Coin1")
-
+    os.chdir("/kernelci-core")
     f = open("config/core/lab-configs.yaml", "a")
     f.write(
     "\n"
@@ -665,13 +663,12 @@ if __name__ == "__main__":
     "    url: 'http://master1'\n"
     "    filters:\n"
     "      - passlist:\n"
-    "        plan:\n"
-    "          - baseline\n")
+    "          plan:\n"
+    "            - baseline\n")
     f.close()
-    print("Coin2")
-    cmd_generate = "./kci_test generate --bmeta-json=/shared_volume/gcc-8_x86_64/1616596107_4.13/_install_/bmeta.json --dtbs-json=/shared_volume/gcc-8_x86_64/1616596107_4.13/_install_/dtbs.json --plan=baseline_qemu --target=qemu_x86_64 --user=admin --lab-config=lab-local --lab-token=8ec4c0aeaf934ed1dce98cdda800c81c --storage=http://storage/ > job_docker.yaml"
+    bmeta_path = os.path.join(install_path, 'bmeta.json')
+    dtbs_path = os.path.join(install_path, 'dtbs.json')
+    cmd_generate = "python3 kci_test generate --bmeta-json=bmeta_path --dtbs-json=dtbs_path --plan=baseline_qemu --target=qemu_x86_64 --user=admin --lab-config=lab-local --lab-token=8ec4c0aeaf934ed1dce98cdda800c81c --storage=http://storage/ > job_docker.yaml"
     os.system(cmd_generate)
-    print("Coin3")
-    cmd_submit = "./kci_test submit --user=admin --lab-config=lab-local --lab-token=8ec4c0aeaf934ed1dce98cdda800c81c --jobs=job_docker.yaml"
+    cmd_submit = "python3 kci_test submit --user=admin --lab-config=lab-local --lab-token=8ec4c0aeaf934ed1dce98cdda800c81c --jobs=job_docker.yaml"
     os.system(cmd_submit)
-    print("Coin4")
