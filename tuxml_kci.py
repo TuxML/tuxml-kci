bedffae094578f9063b93f7fe0a08ab93a00f8d3
bibi14010 <martin.thomas.1@univ-rennes1.fr>
2021-04-07 15:55:19 +0200

    Trying to Create BuildEnvironment Object, again and again

--- tuxml_kci.py
+++ tuxml_kci.py
@@ -646,7 +646,7 @@ if __name__ == "__main__":
 
     current_date = calendar.timegm(time.gmtime())
     output_folder = "/shared_volume/{b_env}_{arch}/{timestamp}_{kver}".format(b_env=b_env, arch=arch, timestamp=current_date, kver=kver)
-    build_env = kciconf.BuildEnvironment(build_config)
+    build_env = kciconf.build.BuildEnvironment(build_config)
     build.build_kernel(build_env,extraction_path,arch)
     #build.build_kernel(b_env=b_env, arch=arch, kdir=extraction_path, defconfig=config, output_path=output_folder)
 
