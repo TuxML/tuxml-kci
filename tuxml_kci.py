7964e081b7b531d55aeeefb9b7bc1e574be8d095
bibi14010 <martin.thomas.1@univ-rennes1.fr>
2021-04-07 17:45:16 +0200

    KCONFIG bug fix

--- tuxml_kci.py
+++ tuxml_kci.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
         path_config = os.getcwd()
         subprocess.call("mkdir ." + kerBuild, shell=True)
         subprocess.call("mv " + config + " ." + kerBuild + "/.config", shell=True)
-        subprocess.call(f'KCONFIG_ALLCONFIG=.{kerBuild}/.config make allnoconfig',shell=True)
+        subprocess.call(f'KCONFIG_ALLCONFIG=.{kerBuild}/.config make randconfig',shell=True)
 
     kernel(os.getcwd() + kerBuild + "/", arch)
     os.chdir("..")
