e391ec406d5823346ea8f177b8b8eaf890d95e0a
bibi14010 <martin.thomas.1@univ-rennes1.fr>
2021-04-17 15:50:48 +0200

    Myconfig progress

--- tuxml_kci.py
+++ tuxml_kci.py
@@ -116,9 +116,9 @@ def build_kernel(kdir, arch, config=None, jopt=None,
         subprocess.call('make mrproper', shell=True)
 
     else:
-        config = None
         os.mkdir(f"{kdir}/build")
         shutil.copy(config, f"{output_path}/.config")
+        config = None
 
         subprocess.call(f'make KCONFIG_ALLCONFIG={output_path}/.config allnoconfig', shell=True)
         subprocess.call(f'make KCONFIG_ALLCONFIG={output_path}/.config alldefconfig', shell=True)
