4d37ffbab710d7a60b573e0ab8c2b9d96b03d65c
bibi14010 <martin.thomas.1@univ-rennes1.fr>
2021-04-19 15:09:54 +0200

    Path error fix

--- tuxml_kci.py
+++ tuxml_kci.py
@@ -127,7 +127,7 @@ def build_kernel(kdir, arch, config=None, jopt=None,
         subprocess.call(f'make KCONFIG_ALLCONFIG={config} allnoconfig', shell=True)
         subprocess.call(f'make KCONFIG_ALLCONFIG={config} alldefconfig', shell=True)
         # Trying to reuse .config made in order to avoid choices
-        shutil.copy(f"./.config", f"{extraction_path}/.config")
+        shutil.copy(f"./.config", f"{output_path}/.config")
         # this step is actually important: it cleans all compiled files due to make rand|tiny|def config
         # otherwise kernel sources are not clean and kci complains
         subprocess.call('make mrproper', shell=True)
